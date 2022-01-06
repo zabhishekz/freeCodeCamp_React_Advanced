3598f3c8b1899b239bfac247fb05256cce63e4de
Abhishek Verma <zabhishek.verma@gmail.com>
2022-01-06 18:56:19 +0530

    useEffect - Basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/5-useState-counter";
+import Setup from "./tutorial/2-useEffect/1-useEffect-basics";
 
 function App() {
   return (
