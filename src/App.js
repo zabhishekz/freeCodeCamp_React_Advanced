bfec219f7163fd96fab9a4c8070a7906cfe4fce3
Abhishek Verma <zabhishek.verma@gmail.com>
2022-01-07 16:43:50 +0530

    Short-Circuit Evaluation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/1-multiple-returns";
+import Setup from "./tutorial/3-conditional-rendering/2-short-circuit";
 
 function App() {
   return (
