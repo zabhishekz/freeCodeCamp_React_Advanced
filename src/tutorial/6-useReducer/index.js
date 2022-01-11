62a5806fcd4c14d91bac082aecfda40a0359496c
Abhishek Verma <zabhishek.verma@gmail.com>
2022-01-11 16:45:55 +0530

    useReducer- Remove Item

--- src/tutorial/6-useReducer/index.js
+++ src/tutorial/6-useReducer/index.js
@@ -3,7 +3,22 @@ import Modal from "./Modal";
 import { data } from "../../data";
 // reducer function
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "item added",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return { ...state, isModalOpen: true, modalContent: "please enter value" };
+  }
+  throw new Error("no matching action type");
+};
+
 const defaultState = {
   people: [],
   isModalOpen: false,
@@ -17,7 +32,11 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
+      setName("");
     } else {
+      dispatch({ type: "NO_VALUE" });
     }
   };
 
